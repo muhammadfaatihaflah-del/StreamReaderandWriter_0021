755ac331b5b0f2bfe99f5a870d066dab75380fa6
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-06-01 10:11:35 +0700

    kondisi jika file ada

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -36,5 +36,11 @@ int main()
     infile.open("contohfile.txt");
 
     cout << endl << ">=Membuka dan membaca file " << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        
+    }    
+
 
 }
